2208b743d30e475061429bc6aa6bf8d52fb8761c
Ebba Birhanu <ebba.birhanu@gmail.com>
2024-06-06 17:00:22 +0300

    updated navlink object for the sidebar

--- src/layout/sidebar/data/nav-links.tsx
+++ src/layout/sidebar/data/nav-links.tsx
@@ -1,8 +1,6 @@
-import { Home, Package, ShoppingCart, Users } from "lucide-react";
+import { Home, User } from "lucide-react";
 
 export const navLinks = [
   { to: "/dashboard", title: "Dashboard", Icon: Home },
-  { to: "/orders", title: "Orders", isBadge: true, Icon: ShoppingCart },
-  { to: "/customers", title: "Customers", Icon: Users },
-  { to: "/products", title: "Products", Icon: Package },
+  { to: "/admins", title: "Admins", isBadge: true, Icon: User },
 ];
