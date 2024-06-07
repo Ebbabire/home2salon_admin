33e8f5b669cc85d12d859660967e0c30f6cda425
Ebba Birhanu <ebba.birhanu@gmail.com>
2024-06-07 15:45:01 +0300

    added reusable admin form for adding and editing admin

--- src/components/error-display.tsx
+++ src/components/error-display.tsx
@@ -5,9 +5,9 @@ const Error = ({ error }: { error: string }) => {
   return (
     <>
       {error && (
-        <div className="flex flex-col gap-2 items-center justify-center h-[50vh]">
+        <div className="flex h-[50vh] flex-col items-center justify-center gap-2">
           <TbFaceIdError size={"24rem"} color="999999" />
-          <span className="text-lg text-[#16243D] dark:text-[#fff] font-medium">
+          <span className="text-lg font-medium text-[#16243D] dark:text-[#fff]">
             {error}
           </span>
           <Button onClick={() => window.location.reload()}>Retry</Button>
