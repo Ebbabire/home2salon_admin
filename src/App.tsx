540559f512838c81fc42d56b0439eaee8e7e0294
ebbabire <ebba.birhanu@gmail.com>
2026-04-01 13:33:14 +0300

    latest update

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-// import { useEffect } from "react";
-// import { useLocation, useNavigate } from "react-router-dom";
-import AllRoutes from "./routes/AllRoutes";
+import { useEffect } from "react"
+import { useLocation, useNavigate } from "react-router-dom"
+import AllRoutes from "./routes/AllRoutes"
 
 function App() {
-  // const token = JSON.parse(sessionStorage.getItem("token")!);
-  // const navigate = useNavigate();
-  // const { pathname } = useLocation();
+  const token = JSON.parse(sessionStorage.getItem("token")!)
+  const navigate = useNavigate()
+  const { pathname } = useLocation()
 
-  // useEffect(() => {
-  //   if (!token && pathname !== "/pass-reset" && pathname !== "/privacyPolicy") {
-  //     navigate("/login");
-  //   }
-  // }, [token, navigate, pathname]);
+  useEffect(() => {
+    if (!token && pathname !== "/pass-reset" && pathname !== "/privacyPolicy") {
+      navigate("/login")
+    }
+  }, [token, navigate, pathname])
   return (
     <>
       <AllRoutes />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
